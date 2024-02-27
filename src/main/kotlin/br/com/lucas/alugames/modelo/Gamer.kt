272604ace5c3265bcb7944cc437cb02ff66d944c
LucasLucky1 <lucaserickson100@gmail.com>
2024-02-26 22:53:00 -0300

    implementando classe gamer e encapsulando atributos

---
+++ src/main/kotlin/br/com/lucas/alugames/modelo/Gamer.kt
@@ -0,0 +1,55 @@
+package br.com.lucas.alugames.modelo
+
+import kotlin.random.Random
+
+class Gamer(var nome: String, var email: String) {
+    var dataNascimento:String? = null
+    var userName: String? = null
+        set(value) {
+            field = value
+            if (userId.isNullOrBlank()){
+                criaIdInterno()
+            }
+
+        }
+    var userId: String? = null
+        private set
+
+    constructor(nome:String, email:String, dataNascimento:String, userName:String):
+            this(nome,email){
+                this.dataNascimento=dataNascimento
+                this.userName=userName
+                criaIdInterno()
+    }
+
+    init {
+        validaNome()
+        this.email= validaEmail()
+    }
+
+    override fun toString(): String {
+        return "Gamer(nome='$nome', email='$email', dataNascimento=$dataNascimento, userName=$userName, userId=$userId)"
+    }
+
+    fun criaIdInterno(){
+        val numeroAleatorio = Random.nextInt(10000)
+        val tag = String.format("%04d", numeroAleatorio)
+
+        userId="$userName#$tag"
+    }
+
+
+    fun validaEmail():String{
+        val regex = Regex("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$")
+        if (regex.matches(email)){
+            return email
+        }else throw IllegalArgumentException("Email Inválido")
+
+    }
+
+    fun validaNome(){
+        if (nome.isNullOrBlank()){
+            throw IllegalArgumentException("Nome Inválido")
+        }
+    }
+}
---
+++ src/main/kotlin/br/com/lucas/alugames/principal/TestandoGamer.kt
@@ -0,0 +1,27 @@
+package br.com.lucas.alugames.principal
+
+import br.com.lucas.alugames.modelo.Gamer
+
+fun main(){
+    val gamer1= Gamer("Jaque", "jaque100@alura.com")
+
+    gamer1.let{
+        it.dataNascimento = "20/10/1996"
+        it.userName = "jaqueSkywalker"
+
+    }.also { println(gamer1) }
+
+
+
+    val gamer2 = Gamer("Jeni", "juni200@alura.com", "20/10/1999", "jeni100" )
+
+    gamer1.let {
+        it.userName = "jack"
+    }.also { println(gamer1) }
+    println(gamer2)
+
+
+
+}
+
+
